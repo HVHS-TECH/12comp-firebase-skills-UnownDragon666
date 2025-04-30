07325a3ad85aa356cdc56cdf01140d4011cf83dc
UnownDragon666 <unown.idrees@gmail.com>
2025-05-01 10:02:00 +1200

    Task 2 done, task 3 WIP

--- fb_io.mjs
+++ fb_io.mjs
@@ -1,7 +1,7 @@
 //**************************************************************/
 // fb_io.mjs
 // Generalised firebase routines
-// Written by <Your Name Here>, Term 2 202?
+// Written by Idrees Munshi, Term 2 2025
 //
 // All variables & function begin with fb_  all const with FB_
 // Diagnostic code lines have a comment appended to them //DIAG
@@ -9,24 +9,39 @@
 const COL_C = 'white';	    // These two const are part of the coloured 	
 const COL_B = '#CD7F32';	//  console.log for functions scheme
 console.log('%c fb_io.mjs',
-            'color: blue; background-color: white;');
+    'color: blue; background-color: white;');
 
 /**************************************************************/
 // Import all external constants & functions required
 /**************************************************************/
 // Import all the methods you want to call from the firebase modules
-
+import { initializeApp }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase }
+    from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 /**************************************************************/
 // EXPORT FUNCTIONS
 // List all the functions called by code or html outside of this module
 /**************************************************************/
-export { 
-    fb_initialise };
+export {
+    fb_initialise,
+    getDatabase,
+    initializeApp
+};
+
+/**************************************************************/
+// fb_initialise()
+// Initialise database (firebase) connection
+// Called by button in index.html
+// Inut:N/A
+// Output: N/A
+/**************************************************************/
+
 
 function fb_initialise() {
-    console.log('%c fb_initialise(): ', 
-                'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    console.log('%c fb_initialise(): ',
+        'color: ' + COL_C + '; background-color: ' + COL_B + ';');
 }
 
 /**************************************************************/
--- main.mjs
+++ main.mjs
@@ -1,7 +1,7 @@
 /**************************************************************/
 // main.mjs
 // Main entry for index.html
-// Written by <Your Name Here>, Term 2 202?
+// Written by Idrees Munshi, Term 2 2025
 /**************************************************************/
 const COL_C = 'white';	    // These two const are part of the coloured 	
 const COL_B = '#CD7F32';	//  console.log for functions scheme
@@ -14,12 +14,14 @@ console.log('%c main.mjs',
 // Import all the constants & functions required from fb_io module
 import { fb_initialise }
     from './fb_io.mjs';
-    window.fb_initialise   = fb_initialise;
+    window.fb_initialise = fb_initialise;
+import { getDatabase }
+    from "./fb_io.mjs";
+    window.getDatabase = getDatabase;
  
 /**************************************************************/
 // index.html main code
 /**************************************************************/
-
 
 /**************************************************************/
 //   END OF CODE
